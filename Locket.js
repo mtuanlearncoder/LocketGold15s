ff41a4c7c7d10e6515c9b7529ef552efe77c88ed
mtuanlearncoder <accphamtuan.xzzz@gmail.com>
2025-12-01 13:24:43 +0700

    Add files via upload

---
+++ Locket.js
@@ -0,0 +1,6 @@
+// ========= ID ========= //
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Module tích hợp hạ cấp của tnetz";var tnetz={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2025-12-1T01:04:18Z",purchase_date:"2025-12-1T01:04:17Z",store:"app_store"},vuong2023={grace_period_expires_date:null,purchase_date:"2025-12-1T01:04:17Z",product_identifier:"com.tnetz.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(vuong2023.product_identifier=s,obj.subscriber.subscriptions[s]=tnetz):obj.subscriber.subscriptions["com.tnetz.premium.yearly"]=tnetz,obj.subscriber.entitlements[e]=vuong2023}else obj.subscriber.subscriptions["com.tnetz.premium.yearly"]=tnetz,obj.subscriber.entitlements.pro=vuong2023;$done({body:JSON.stringify(obj)});
